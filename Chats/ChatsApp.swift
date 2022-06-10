a71470189c1cedd87ee8ef6691e40c799d274b9a
wuqiang <545266262@qq.com>
2022-06-10 21:51:49 +0800

    Initial Commit

---
+++ Chats/ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsApp.swift
+//  Chats
+//
+//  Created by wuqiang on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
